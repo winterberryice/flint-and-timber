8bd24d2c0c117da8590e455b7b8a4ef5ae119ec8
kacper.sledz <casper.sledx@gmail.com>
2025-08-26 23:17:02 +0200

    window attempt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
+const uint32_t kWidth = 512;
+const uint32_t kHeight = 512;
+
+void Start()
+{
+    if (!glfwInit())
+    {
+        return;
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow *window =
+        glfwCreateWindow(kWidth, kHeight, "WebGPU window", nullptr, nullptr);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        // TODO: Render a triangle using WebGPU.
+    }
+}
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    Start();
     return 0;
 }
